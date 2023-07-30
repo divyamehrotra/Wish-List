c5bf96cb015a577ee72db9c03909432bf970df45
Divya Mehrotra <mehrotradivya54@gmail.com>
2023-07-30 19:58:38 +0530

    initial commit

---
+++ signin.js
@@ -0,0 +1,14 @@
+const signinForm = document.getElementById("signin-form");
+
+signinForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Get the values from the form
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Perform validation and sign-in logic here (not implemented in this example)
+  // For demonstration purposes, we'll just log the values to the console.
+  console.log("Username:", username);
+  console.log("Password:", password);
+});
